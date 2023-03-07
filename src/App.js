e7ee31586bf7bffee6528e5810d4f40f82fbc469
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-07 14:49:16 +0530

    Feat: added routes page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import HomePage from "Pages/HomePage";
+import LoginPage from "Pages/LoginPage";
+import ProfilePage from "Pages/ProfilePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+
+
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
