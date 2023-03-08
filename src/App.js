c2c7cba8a7e2fb074dcf611f1a5b25ab52693280
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-08 16:11:30 +0530

    Feat: added navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
+// import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles"
 import HomePage from "Pages/HomePage";
 import LoginPage from "Pages/LoginPage";
 import ProfilePage from "Pages/ProfilePage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
 
 function App() {
-
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
+
+      <Router>
+
+        <ThemeProvider theme={theme} >
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </Router>
     </div>
   );
 }
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <div>hiii</div>;
 };
 
 export default LoginPage;
